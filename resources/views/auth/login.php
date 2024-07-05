e09f0f787ec451cc94e5f93e3860269e4349d8fa
RaesakaDp <kakabuleng@gmail.com>
2024-07-05 19:29:04 +0700

    login page

--- resources/views/auth/login.php
+++ resources/views/auth/login.php
@@ -80,18 +80,18 @@
 
     <!-- ///////////// Js Files ////////////////////  -->
     <!-- Jquery -->
-    <script src="{{'assets/js/lib/jquery-3.4.1.min.js')}}"></script>
+    <script src="{{ asset('assets/js/lib/jquery-3.4.1.min.js')}}"></script>
     <!-- Bootstrap-->
-    <script src="{{'assets/js/lib/popper.min.js')}}"></script>
-    <script src="{{'assets/js/lib/bootstrap.min.js')}}"></script>
+    <script src="{{ asset('assets/js/lib/popper.min.js')}}"></script>
+    <script src="{{ asset('assets/js/lib/bootstrap.min.js')}}"></script>
     <!-- Ionicons -->
     <script type="module" src="https://unpkg.com/ionicons@5.0.0/dist/ionicons/ionicons.js"></script>
     <!-- Owl Carousel -->
-    <script src="{{'assets/js/plugins/owl-carousel/owl.carousel.min.js')}}"></script>
+    <script src="{{ asset ('assets/js/plugins/owl-carousel/owl.carousel.min.js')}}"></script>
     <!-- jQuery Circle Progress -->
-    <script src="{{'assets/js/plugins/jquery-circle-progress/circle-progress.min.js')}}"></script>
+    <script src="{{ asset('assets/js/plugins/jquery-circle-progress/circle-progress.min.js')}}"></script>
     <!-- Base Js File -->
-    <script src="{{'assets/js/base.js')}}"></script>
+    <script src="{{ asset('assets/js/base.js')}}"></script>
 
 
 </body>
