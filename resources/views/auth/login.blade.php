543cec6cd10816ce6d6e22d5512bc76d2649ffd4
RaesakaDp <kakabuleng@gmail.com>
2024-07-07 11:59:53 +0700

    start dashboard

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -31,11 +31,11 @@
 
         <div class="login-form mt-1">
             <div class="section">
-                <img src="assets/img/sample/photo/vector4.png" alt="image" class="form-image">
+                <img src="{{asset ('assets/img/login-page.jpg')}}" alt="image" class="form-image">
             </div>
             <div class="section mt-1">
-                <h1>Get started</h1>
-                <h4>Fill the form to log in</h4>
+                <h1>Absen Dulu Gasi</h1>
+                <h4>Silakan Login Bro!</h4>
             </div>
             <div class="section mt-1 mb-5">
                 <form action="app-pages.html">
@@ -57,10 +57,7 @@
                         </div>
                     </div>
 
-                    <div class="form-links mt-2">
-                        <div>
-                            <a href="page-register.html">Register Now</a>
-                        </div>
+                    <div class="form-links mt-2">                  
                         <div><a href="page-forgot-password.html" class="text-muted">Forgot Password?</a></div>
                     </div>
 
